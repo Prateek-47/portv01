f1778fb55f72c605ca124d1047679ba87954abdf
Prateek_47 <shrmapratik11@gmail.com>
2024-02-23 15:52:14 +0530

    second commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './index.scss';
 import { Link } from 'react-router-dom';
-import logoS from '../../assets/images/logo-s.png'
+import logoS from '../../assets/images/Plogotransparent.png'
+import logosubtitle from '../../assets/images/logo_sub.png';
+
 
 const Sidebar = () => {
   return (
     <div className='nav-bar'>
         <Link className='logo' to="/">
             <img src={logoS} alt='logo'></img>
+            <img src ={logosubtitle} alt='logoname'></img>
         </Link>
     </div>
   )
